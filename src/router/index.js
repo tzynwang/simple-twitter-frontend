10777edde22c01cd4358991078c89da1a788a25b
tzyn (windows) <tzyn.wang@gmail.com>
2021-09-06 18:32:11 +0800

    feat: basic route settings

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,97 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Root',
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: () => import('../views/Settings.vue')
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    caseSensitive: true,
+    redirect: '/admin/tweets',
+    component: () => import('../views/Admin.vue'),
+    children: [
+      {
+        path: 'tweets',
+        name: 'AdminAllTweets',
+        component: () => import('../views/AdminAllTweets.vue')
+      },
+      {
+        path: 'users',
+        name: 'AdminAllUsers',
+        component: () => import('../views/AdminAllUsers.vue')
+      },
+      {
+        path: '*',
+        redirect: '/admin/tweets'
+      }
+    ]
+  },
+  {
+    path: '/:userId',
+    component: () => import('../views/Portfolio.vue'),
+    children: [
+      {
+        path: '/',
+        name: 'Portfolio',
+        component: () => import('../views/PortfolioAllTweets.vue')
+      },
+      {
+        path: 'replies',
+        component: () => import('../views/PortfolioReplies.vue')
+      },
+      {
+        path: 'likes',
+        component: () => import('../views/PortfolioLikes.vue')
+      },
+      {
+        path: 'following',
+        component: () => import('../views/PortfolioFollowing.vue')
+      },
+      {
+        path: 'followers',
+        component: () => import('../views/PortfolioFollowers.vue')
+      },
+      {
+        path: '*',
+        redirect: '/:userId'
+      }
+    ]
+  },
+  {
+    path: '/:userId/:tweetId',
+    name: 'Tweet',
+    component: () => import('../views/ReplyTweet.vue')
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: () => import('../views/NotFound.vue')
   }
 ]
 
