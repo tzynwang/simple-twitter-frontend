a61736ffe2d73011c0ed2f46ec6624170887595e
tzyn (windows) <tzyn.wang@gmail.com>
2021-09-06 22:46:47 +0800

    feat: set formHeader basic structure

---
+++ src/components/formHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img src='@/assets/images/logo.svg' alt="logo">
+    <div>{{ fullPath | title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'formHeader',
+  created () {
+    const { fullPath } = this.$route
+    this.fullPath = fullPath
+  },
+  data () {
+    return {
+      fullPath: ''
+    }
+  },
+  filters: {
+    title (fullPath) {
+      switch (fullPath) {
+        case '/login':
+          return '登入 Alphitter'
+        case '/register':
+          return '建立你的帳號'
+        case '/admin/login':
+          return '後台登入'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
