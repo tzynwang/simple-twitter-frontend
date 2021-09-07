5e39a80db7df90ec7d4be1441c5e185b081d4f12
tzyn (windows) <tzyn.wang@gmail.com>
2021-09-07 16:35:33 +0800

    feat: Settings basic structure

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>
-    setting page
-  </div>
+  <main class="container">
+    <template v-if="windowWidth < 768">
+      <navTop />
+      <p>body Settings page</p>
+      <navBottom />
+    </template>
+    <template v-else-if="windowWidth >= 768 && windowWidth < 1200">
+      <p>left nav</p>
+      <p>body Settings page</p>
+    </template>
+    <template v-else>
+      <p>left nav</p>
+      <p>body Settings page</p>
+      <p>right section</p>
+    </template>
+  </main>
 </template>
 
 <script>
+import navTop from './../components/navTop'
+import navBottom from './../components/navBottom'
+
+import { mapState } from 'vuex'
+
 export default {
-  name: 'Setting'
+  name: 'Setting',
+  components: {
+    navTop,
+    navBottom
+  },
+  computed: {
+    ...mapState(['windowWidth'])
+  }
 }
 </script>
