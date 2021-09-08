f5ed2a52543ad23352a7058c4cd5545c59ee2fd7
tzyn (windows) <tzyn.wang@gmail.com>
2021-09-08 22:50:55 +0800

    feat: set forms style

--- src/components/registerForm.vue
+++ src/components/registerForm.vue
@@ -1,32 +1,38 @@
 <template>
-  <form @submit.prevent="formSubmit">
-    <div>
-      <input type="text" id="account" name="account">
+  <form class="mt-20" @submit.prevent="formSubmit">
+    <div
+      :class="['input-group', 'input-account', { 'input-error': accountError }]"
+    >
+      <input type="text" id="account" name="account" />
       <label for="account">帳號</label>
-      <div class="error-message"></div>
+      <div class="error-message" v-show="accountError">帳號已重覆註冊</div>
     </div>
-    <div>
-      <input type="text" id="name" name="name">
+    <div :class="['input-group', { 'input-error': nameError }]">
+      <input type="text" id="name" name="name" />
       <label for="name">名稱</label>
-      <div class="error-message"></div>
+      <div class="error-message" v-show="nameError"></div>
     </div>
-    <div>
-      <input type="email" id="email" name="email">
+    <div :class="['input-group', { 'input-error': emailError }]">
+      <input type="email" id="email" name="email" />
       <label for="email">Email</label>
-      <div class="error-message"></div>
+      <div class="error-message" v-show="emailError">Email已重覆註冊</div>
     </div>
-    <div>
-      <input type="password" id="password" name="password">
+    <div :class="['input-group', { 'input-error': passwordError }]">
+      <input type="password" id="password" name="password" />
       <label for="password">密碼</label>
-      <div class="error-message"></div>
+      <div class="error-message" v-show="passwordError">
+        密碼與密碼確認內容不一致
+      </div>
     </div>
-    <div>
-      <input type="password" id="passwordConfirm" name="passwordConfirm">
+    <div class="input-group">
+      <input type="password" id="passwordConfirm" name="passwordConfirm" />
       <label for="passwordConfirm">密碼確認</label>
       <div class="error-message"></div>
     </div>
-    <button :disabled="isProcessing">{{ registerButtonText }}</button>
-    <router-link :to="{ name: 'Login' }">取消</router-link>
+    <button class="btn btn-primary btn-form-submit" :disabled="isProcessing">
+      {{ registerButtonText }}
+    </button>
+    <router-link class="pre-login-link text-center mt-20" :to="{ name: 'Login' }">取消</router-link>
   </form>
 </template>
 
@@ -35,7 +41,11 @@ export default {
   name: 'registerForm',
   data () {
     return {
-      isProcessing: false
+      isProcessing: false,
+      accountError: false,
+      nameError: false,
+      emailError: false,
+      passwordError: false
     }
   },
   methods: {
@@ -52,5 +62,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/components/settingForm.vue
+++ src/components/settingForm.vue
@@ -1,32 +1,39 @@
 <template>
-  <form @submit.prevent="formSubmit">
-    <div>
-      <input type="text" id="account" name="account">
+  <form class="mt-30 container-setting-form" @submit.prevent="formSubmit">
+    <div
+      :class="['input-group', 'input-account', { 'input-error': accountError }]"
+    >
+      <input type="text" id="account" name="account" />
       <label for="account">帳號</label>
-      <div class="error-message"></div>
+      <div class="error-message" v-show="accountError">此帳號已經被使用</div>
     </div>
-    <div>
-      <input type="text" id="name" name="name">
+    <div :class="['input-group', { 'input-error': nameError }]">
+      <input type="text" id="name" name="name" />
       <label for="name">名稱</label>
-      <div class="error-message"></div>
+      <div class="error-message" v-show="nameError"></div>
     </div>
-    <div>
-      <input type="email" id="email" name="email">
+    <div :class="['input-group', { 'input-error': emailError }]">
+      <input type="email" id="email" name="email" />
       <label for="email">Email</label>
-      <div class="error-message"></div>
+      <div class="error-message" v-show="emailError">此Email已經被使用</div>
     </div>
-    <div>
-      <input type="password" id="password" name="password">
+    <div :class="['input-group', { 'input-error': passwordError }]">
+      <input type="password" id="password" name="password" />
       <label for="password">密碼</label>
-      <div class="error-message"></div>
+      <div class="error-message" v-show="passwordError">
+        密碼與密碼確認內容不一致
+      </div>
     </div>
-    <div>
-      <input type="password" id="passwordConfirm" name="passwordConfirm">
+    <div class="input-group">
+      <input type="password" id="passwordConfirm" name="passwordConfirm" />
       <label for="passwordConfirm">密碼確認</label>
       <div class="error-message"></div>
     </div>
-    <button :disabled="isProcessing">{{ registerButtonText }}</button>
-    <router-link :to="{ name: 'Login' }">取消</router-link>
+    <div class="text-right">
+      <button class="btn btn-primary btn-form-save" :disabled="isProcessing">
+        {{ registerButtonText }}
+      </button>
+    </div>
   </form>
 </template>
 
@@ -35,7 +42,11 @@ export default {
   name: 'settingForm',
   data () {
     return {
-      isProcessing: false
+      isProcessing: false,
+      accountError: false,
+      nameError: false,
+      emailError: false,
+      passwordError: false
     }
   },
   methods: {
@@ -52,5 +63,4 @@ export default {
 </script>
 
 <style>
-
 </style>
