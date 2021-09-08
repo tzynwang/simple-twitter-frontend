c9540738eafabe862b695c805278c1dbebba5e36
tzyn (windows) <tzyn.wang@gmail.com>
2021-09-08 10:48:56 +0800

    feat: AdminAllTweets/AllUsers basic structure

--- src/views/AdminAllTweets.vue
+++ src/views/AdminAllTweets.vue
@@ -1,12 +1,39 @@
 <template>
-  <div>
-    admin all tweets page
-  </div>
+  <main class="container">
+    <template v-if="windowWidth < 768">
+      <navTop />
+      <section class="container-body">
+        <p>body admin all tweet page</p>
+      </section>
+      <navBottomAdmin />
+    </template>
+    <template v-else-if="windowWidth >= 768 && windowWidth < 1200">
+      <p>left nav</p>
+      <p>body admin all tweet page</p>
+    </template>
+    <template v-else>
+      <p>left nav</p>
+      <p>body admin all tweet page</p>
+      <p>right section</p>
+    </template>
+  </main>
 </template>
 
 <script>
+import navTop from './../components/navTop'
+import navBottomAdmin from './../components/navBottomAdmin'
+
+import { mapState } from 'vuex'
+
 export default {
-  name: 'AdminAllTweets'
+  name: 'AdminAllTweets',
+  components: {
+    navTop,
+    navBottomAdmin
+  },
+  computed: {
+    ...mapState(['windowWidth'])
+  }
 }
 </script>
 
