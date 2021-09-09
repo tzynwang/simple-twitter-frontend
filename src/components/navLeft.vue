e802e931c7080b4f8f64cfcaca6687503dce1fee
tzyn (windows) <tzyn.wang@gmail.com>
2021-09-09 15:05:11 +0800

    feat: add navLeft (tablet, desktop, admin)

--- src/components/navLeft.vue
+++ src/components/navLeft.vue
@@ -1,13 +1,53 @@
 <template>
-  <nav>left side nav in tablet</nav>
+  <nav class="nav-left">
+    <ul>
+      <li>
+        <img src="@/assets/images/logo.svg" alt="logo" @click="toHome" />
+      </li>
+      <li>
+        <router-link :to="{ name: 'Home' }">
+          <img src="@/assets/images/nav-home.svg" alt="home" />
+        </router-link>
+      </li>
+      <li>
+        <router-link
+          :to="{ name: 'UserAllTweets', params: { userAccount: 'userId' } }"
+        >
+          <img src="@/assets/images/nav-user.svg" alt="user" />
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'Settings' }">
+          <img src="@/assets/images/nav-setting.svg" alt="settings" />
+        </router-link>
+      </li>
+      <li>
+        <button class="btn btn-primary btn-round">
+          <img src="@/assets/images/nav-tweet.svg" alt="" />
+        </button>
+      </li>
+    </ul>
+    <button class="btn btn-left-nav-logout">
+      <img src="@/assets/images/nav-logout.svg" alt="logout" />
+    </button>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: 'navLeft'
+  name: 'navLeft',
+  methods: {
+    toHome () {
+      if (this.$route.fullPath === '/home') return
+      this.$router.push({ name: 'Home' })
+      /*
+      這邊不使用router-link實作是因為.router-link-active會給img加上橙色filter
+      故設計成點擊img後推回/home
+      */
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
--- src/components/navLeftDesktop.vue
+++ src/components/navLeftDesktop.vue
@@ -1,13 +1,55 @@
 <template>
-  <nav>left side nav in desktop (menu with text)</nav>
+  <nav class="nav-left">
+    <ul>
+      <li>
+        <img src="@/assets/images/logo.svg" alt="logo" @click="toHome" />
+      </li>
+      <li>
+        <router-link :to="{ name: 'Home' }">
+          <img src="@/assets/images/nav-home.svg" alt="home" />
+          <span>首頁</span>
+        </router-link>
+      </li>
+      <li>
+        <router-link
+          :to="{ name: 'UserAllTweets', params: { userAccount: 'userId' } }"
+        >
+          <img src="@/assets/images/nav-user.svg" alt="user" />
+          <span>個人資料</span>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'Settings' }">
+          <img src="@/assets/images/nav-setting.svg" alt="settings" />
+          <span>設定</span>
+        </router-link>
+      </li>
+      <li>
+        <button class="btn btn-primary btn-left-nav-new-tweet">推文</button>
+      </li>
+    </ul>
+    <button class="btn btn-left-nav-logout">
+      <img src="@/assets/images/nav-logout.svg" alt="logout" />
+      <span>登出</span>
+    </button>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: 'navLeftDesktop'
+  name: 'navLeftDesktop',
+  methods: {
+    toHome () {
+      if (this.$route.fullPath === '/home') return
+      this.$router.push({ name: 'Home' })
+      /*
+      這邊不使用router-link實作是因為.router-link-active會給img加上橙色filter
+      故設計成點擊img後推回/home
+      */
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
