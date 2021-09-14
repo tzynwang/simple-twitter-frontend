fb2c1b8660261f6e3f2461336a3cac3abe5cf689
tzyn.wang <tzyn.wang@gmail.com>
2021-09-14 16:44:16 +0800

    feat: registerForm pseudocode

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,11 +9,22 @@
 import formHeader from './../components/formHeader'
 import loginForm from './../components/loginForm'
 
+import { successToast } from './../utils/toasts'
+
 export default {
   name: 'Login',
   components: {
     formHeader,
     loginForm
+  },
+  beforeRouteEnter (to, from, next) {
+    const { register } = to.params
+    if (register === 'success') {
+      successToast.fire({
+        title: '註冊成功'
+      })
+    }
+    next()
   }
 }
 </script>
