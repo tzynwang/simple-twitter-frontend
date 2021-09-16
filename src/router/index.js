e9b7d24f7ff4788177a56da1ec8c401e461f602b
tzyn.wang <tzyn.wang@gmail.com>
2021-09-16 22:39:26 +0800

    feat: router.beforeEach token, authorization check

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import store from '@/store'
+import authorization from '@/store/modules/authorization'
+
+import userAPI from '@/apis/user'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -102,6 +107,37 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  const tokenInLocalStorage = window.localStorage.getItem('simpleTweeterToken')
+  const tokenInStore = authorization.state.token
+  let isAuthenticated = authorization.state.isAuthenticated
+
+  // 先比較localStorage與vuex的token是否「不同」（先 !== 再 &&）
+  // 兩邊不同，且localStorage「有token」的話，透過 getCurrentUser 用token交換該使用者的資料
+  if (tokenInLocalStorage && tokenInLocalStorage !== tokenInStore) {
+    const { data } = await userAPI.getCurrentUser()
+    store.dispatch('setUser', data, { root: true })
+    isAuthenticated = authorization.state.isAuthenticated
+  }
+
+  // 列出不需要驗證 token 的頁面
+  const pathsWithoutAuthentication = ['Login', 'Register', 'AdminLogin']
+
+  // 如果 token 無效且又想進入需要驗證的頁面，轉到/login
+  if (!isAuthenticated && !pathsWithoutAuthentication.includes(to.name)) {
+    next('/login')
+    return
+  }
+
+  // 如果 token 有效且又想進入登入註冊頁面，轉到/home
+  if (isAuthenticated && pathsWithoutAuthentication.includes(to.name)) {
+    next('/home')
+    return
+  }
+
+  next()
 })
 
 export default router
