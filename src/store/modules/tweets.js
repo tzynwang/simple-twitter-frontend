974c35b39a7f038b62e1533778d29122d963dab7
tzyn.wang <tzyn.wang@gmail.com>
2021-09-16 22:37:50 +0800

    feat: vuex add tweets module

---
+++ src/store/modules/tweets.js
@@ -0,0 +1,64 @@
+const state = {
+  tweets: []
+}
+
+const getters = {
+  getTweets: state => state.tweets
+}
+
+const actions = {
+  setTweets ({ commit }, tweets) {
+    commit('updateTweets', tweets)
+  },
+  addNewTweet ({ commit }, tweet) {
+    commit('addNewTweet', tweet)
+  },
+  addLikeToTweet ({ commit }, tweetId) {
+    commit('addLikeToTweet', tweetId)
+  },
+  removeLikeFromTweet ({ commit }, tweetId) {
+    commit('removeLikeFromTweet', tweetId)
+  },
+  addReplyToTweet ({ commit }, tweetId) {
+    commit('addReplyToTweet', tweetId)
+  }
+}
+
+const mutations = {
+  updateTweets: (state, tweets) => {
+    state.tweets = tweets
+  },
+  addNewTweet: (state, tweet) => {
+    state.tweets.unshift(tweet) // 越新的推文index越小，故使用unshift
+  },
+  addLikeToTweet: (state, tweetId) => {
+    state.tweets.forEach(tweet => {
+      if (tweet.id === tweetId) {
+        tweet.totalLike += 1
+        tweet.isLiked = !tweet.isLiked
+      }
+    })
+  },
+  removeLikeFromTweet: (state, tweetId) => {
+    state.tweets.forEach(tweet => {
+      if (tweet.id === tweetId) {
+        tweet.totalLike -= 1
+        tweet.isLiked = !tweet.isLiked
+      }
+    })
+  },
+  addReplyToTweet: (state, tweetId) => {
+    state.tweets.forEach(tweet => {
+      if (tweet.id === tweetId) {
+        tweet.totalReply += 1
+      }
+    })
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
