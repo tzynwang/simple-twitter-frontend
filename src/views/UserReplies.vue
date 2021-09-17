03b8f6c566c5c0bf6edd2da826acf10ba803d490
tzyn.wang <tzyn.wang@gmail.com>
2021-09-17 14:17:48 +0800

    feat: UsreReplies connect with API

--- src/views/UserReplies.vue
+++ src/views/UserReplies.vue
@@ -1,13 +1,20 @@
 <template>
   <section class="container-body-tablet-desktop">
     <navTopArrowTweetsCount
-      :user-name="userName"
-      :tweet-counts="tweetCounts"
+      :user-name="getUser.name"
+      :tweet-counts="getCurrentUserTweets.length"
     />
     <section class="container-body">
       <userProfile />
       <tweetTab />
-      <replyFromUser />
+      <replyFromUser
+        v-for="(reply, index) in userReplies"
+        :key="index"
+        :reply="reply"
+        :user-name="getUser.name"
+        :user-account="getUser.account"
+        :user-avatar="getUser.avatar"
+      />
     </section>
   </section>
 </template>
@@ -18,41 +25,54 @@ import userProfile from './../components/userProfile'
 import tweetTab from './../components/tweetTab'
 import replyFromUser from './../components/replyFromUser'
 
-import { mapState } from 'vuex'
+import { failToast } from '@/utils/toasts'
+import userAPI from '@/apis/user'
+import { fetchAllTweetsMixins } from '@/utils/mixins'
+
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   name: 'UserReplies',
+  mixins: [fetchAllTweetsMixins],
   components: {
     navTopArrowTweetsCount,
     userProfile,
     tweetTab,
     replyFromUser
   },
-  created () {
-    // get userId by route
-    const { fullPath } = this.$route
-    this.fullPath = fullPath
-  },
-  beforeRouteUpdate (to, from, next) {
-    // get userId by route
-    this.fullPath = to.fullPath
-    next()
-  },
   data () {
     return {
-      fullPath: ''
+      userReplies: []
+    }
+  },
+  created () {
+    // 為了取使用者的推文數量，必定需要取全部的推文
+    this.fetchAllTweets()
+    this.getCurrentUserReplies()
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.fetchAllTweets()
+    this.getCurrentUserReplies()
+    next()
+  },
+  methods: {
+    async getCurrentUserReplies () {
+      try {
+        const { data } = await userAPI.getCurrentUserRepliedTweets(
+          this.getUser.id
+        )
+        this.userReplies = data
+      } catch (error) {
+        console.error(error)
+        failToast.fire({
+          title: '無法取得推文，請稍候再試'
+        })
+      }
     }
   },
   computed: {
     ...mapState(['windowWidth']),
-    userName () {
-      // TODO: get userName by userId
-      return this.fullPath.split('/')[1]
-    },
-    tweetCounts () {
-      // TODO: get user tweet counts by userId
-      return 'xxxx'
-    }
+    ...mapGetters(['getCurrentUserTweets', 'getUser'])
   }
 }
 </script>
