3ecd4ba6c45fe4e262c6e3e7b53e01456da8f8e5
tzyn.wang <tzyn.wang@gmail.com>
2021-09-17 14:21:29 +0800

    feat: replyFromUser connect with API

--- src/components/replyFromUser.vue
+++ src/components/replyFromUser.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="tweet-component">
     <div class="tweet-component-avatar">
-      <img class="avatar-img mt-18 ml-15 mr-10" src="" alt="user avatar" />
+      <img
+        class="avatar-img mt-18 ml-15 mr-10"
+        :src="userAvatar"
+        alt="user avatar"
+      />
     </div>
     <div class="tweet-component-content pr-15">
       <div class="header mt-15">
-        <span class="user-name mr-5">userName</span>
-        <span class="user-account">{{ "aUserAccount" | userAccount }}</span>
-        <span class="time-stamp">timeStamp</span>
+        <span class="user-name mr-5">{{ userName }}</span>
+        <span class="user-account">{{ userAccount | userAccount }}</span>
+        <span class="time-stamp">{{ reply.createdAt | fromNow }}</span>
       </div>
       <div class="mt-4 text-gray">
-        回覆 <span class="text-primary">{{ "aUserAccount" | userAccount }}</span>
+        回覆
+        <span class="text-primary">{{
+          reply.Tweet.User.name | userAccount
+        }}</span>
       </div>
       <div class="body mt-6 mb-15">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum
-        corporis culpa cumque, ut dolorem voluptates assumenda temporibus
-        laboriosam quae quibusdam fugiat vero amet omnis tenetur ea deserunt
-        accusantium? Porro.
+        {{ reply.comment }}
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { accountStringFilter } from '@/utils/mixins'
+import { accountStringFilter, timeFilter } from '@/utils/mixins'
 
 export default {
   name: 'replyFromUser',
-  mixins: [accountStringFilter]
+  mixins: [accountStringFilter, timeFilter],
+  props: ['userName', 'userAccount', 'userAvatar', 'reply']
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
