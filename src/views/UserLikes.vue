def65b514cf881530cb2c11dc50fc30c888292cd
tzyn.wang <tzyn.wang@gmail.com>
2021-09-17 19:36:48 +0800

    feat: UserLikes get tweets by id in URL

--- src/views/UserLikes.vue
+++ src/views/UserLikes.vue
@@ -1,15 +1,15 @@
 <template>
   <section class="container-body-tablet-desktop">
     <navTopArrowTweetsCount
-      :user-name="getUser.name"
-      :tweet-counts="getCurrentUserTweets.length"
+      :user-name="getUserByIdVuex.name"
+      :tweet-counts="getUserByIdVuex.totalTweets"
     />
     <section class="container-body">
       <userProfile />
       <tweetTab />
-      <tweet
-        v-for="tweet in getCurrentUserLikedTweets"
-        :key="tweet.id"
+      <tweetInUserLikes
+        v-for="tweet in getLikesByUserIdVuex"
+        :key="tweet.TweetId"
         :tweet="tweet"
       />
     </section>
@@ -20,36 +20,42 @@
 import navTopArrowTweetsCount from './../components/navTopArrowTweetsCount'
 import userProfile from './../components/userProfile'
 import tweetTab from './../components/tweetTab'
-import tweet from './../components/tweet'
+import tweetInUserLikes from './../components/tweetInUserLikes'
 
 import { mapState, mapGetters } from 'vuex'
 
-import { fetchAllTweetsMixins } from '@/utils/mixins'
+import {
+  fetchAllTweetsMixins,
+  fetchUserByIdInPathMixins
+} from '@/utils/mixins'
 
 export default {
   name: 'UserLikes',
-  mixins: [fetchAllTweetsMixins],
+  mixins: [fetchAllTweetsMixins, fetchUserByIdInPathMixins],
   components: {
     navTopArrowTweetsCount,
     userProfile,
     tweetTab,
-    tweet
+    tweetInUserLikes
+  },
+  data () {
+    return {
+      currentPathId: -1
+    }
   },
   created () {
-    // get all tweets
-    this.fetchAllTweets()
+    // 透過路由取id，再使用該id取得所有讚過的推文
+    this.getUserById(this.$route.params.userAccount)
+    this.getAllLikesByUserId(this.$route.params.userAccount)
   },
   beforeRouteUpdate (to, from, next) {
-    this.fetchAllTweets()
+    this.getUserById(to.params.userAccount)
+    this.getAllLikesByUserId(to.params.userAccount)
     next()
   },
   computed: {
     ...mapState(['windowWidth']),
-    ...mapGetters([
-      'getCurrentUserTweets',
-      'getCurrentUserLikedTweets',
-      'getUser'
-    ])
+    ...mapGetters(['getLikesByUserIdVuex', 'getUserByIdVuex'])
   }
 }
 </script>
