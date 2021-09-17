536c18f7b12fb5596ee594f42698159e94d55846
tzyn.wang <tzyn.wang@gmail.com>
2021-09-17 14:21:12 +0800

    feat: apis add getCurrentUserRepliedTweets

--- src/apis/user.js
+++ src/apis/user.js
@@ -4,6 +4,9 @@ export default {
   getCurrentUser () {
     return apiHelper.get('/users/current')
   },
+  getCurrentUserRepliedTweets (currentUserId) {
+    return apiHelper.get(`/users/${currentUserId}/replied_tweets`)
+  },
   addLikeToTweet (tweetId) {
     return apiHelper.post(`/tweets/${tweetId}/like`)
   },
