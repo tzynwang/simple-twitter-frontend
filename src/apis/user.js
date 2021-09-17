d6224203896df66c227fb0ee67249f0b6ab36904
tzyn.wang <tzyn.wang@gmail.com>
2021-09-17 19:34:04 +0800

    feat: apis user add get data by id routes getUserById getAllTweetsById getAllRepliesById getAllLikesById

--- src/apis/user.js
+++ src/apis/user.js
@@ -12,5 +12,17 @@ export default {
   },
   removeLikeFromTweet (tweetId) {
     return apiHelper.post(`/tweets/${tweetId}/unlike`)
+  },
+  getUserById (userId) {
+    return apiHelper.get(`/users/${userId}`)
+  },
+  getAllTweetsById (userId) {
+    return apiHelper.get(`/users/${userId}/tweets`)
+  },
+  getAllRepliesById (userId) {
+    return apiHelper.get(`/users/${userId}/replied_tweets`)
+  },
+  getAllLikesById (userId) {
+    return apiHelper.get(`/users/${userId}/likes`)
   }
 }
