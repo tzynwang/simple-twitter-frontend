0223be5e845218970296e4219cdae086e9d278d8
tzyn.wang <tzyn.wang@gmail.com>
2021-09-18 19:45:28 +0800

    feat: apis tweet add getTweet, getTweetAllReplies

--- src/apis/tweet.js
+++ src/apis/tweet.js
@@ -4,6 +4,12 @@ export default {
   getAllTweets () {
     return apiHelper.get('/tweets')
   },
+  getTweet (tweetId) {
+    return apiHelper.get(`/tweets/${tweetId}`)
+  },
+  getTweetAllReplies (tweetId) {
+    return apiHelper.get(`/tweets/${tweetId}/replies`)
+  },
   addNewTweet (newTweet) {
     return apiHelper.post('/tweets', newTweet)
   },
