61888a0345dee3b7a4fd41e35f4059cfb9fd881e
tzyn.wang <tzyn.wang@gmail.com>
2021-09-18 15:38:04 +0800

    feat: vuex follows module add add/remove follow

--- src/store/modules/follows.js
+++ src/store/modules/follows.js
@@ -9,12 +9,34 @@ const getters = {
 const actions = {
   setFollowing ({ commit }, following) {
     commit('updateFollowing', following)
+  },
+  addFollowing ({ commit }, newFollowing) {
+    commit('addFollowing', newFollowing)
+  },
+  removeFollowing ({ commit }, userId) {
+    commit('removeFollowing', userId)
   }
 }
 
 const mutations = {
   updateFollowing: (state, following) => {
     state.following = following
+  },
+  addFollowing: (state, newFollowing) => {
+    const result = state.following.filter(user => user.followingId === newFollowing.followingId)
+    // 已經存在state.following中的話，修改isFollowings狀態即可，反之則新增該追蹤者至state.following中
+    if (result.length) {
+      result[0].isFollowings = true
+    } else {
+      state.following.push(newFollowing)
+    }
+  },
+  removeFollowing: (state, userId) => {
+    state.following.forEach(user => {
+      if (user.followingId === userId) {
+        user.isFollowings = false
+      }
+    })
   }
 }
 
