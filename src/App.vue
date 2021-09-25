d3028dbb1347300fae15517a593521d8ef4c73cf
ravenera0317 <r6090408@gmail.com>
2021-09-25 22:42:52 +0800

    feat: connect socket in App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,36 @@
 </template>
 
 <script>
+import io from 'socket.io-client'
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'App',
+  data () {
+    return {
+      socket: {}
+    }
+  },
+  computed: {
+    ...mapGetters(['getUser', 'getToken', 'getAuthenticated'])
+  },
   created () {
     this.getWindowWidth()
     window.addEventListener('resize', this.getWindowWidth)
+
+    // 在登入的狀態下才與 socket 建立連線
+    if (this.getAuthenticated) {
+      this.socket = io('https://c60e-27-240-218-37.ngrok.io', {
+        query: {
+          id: this.getUser.id,
+          name: this.getUser.name,
+          avatar: this.getUser.avatar,
+          account: this.getUser.account
+        }
+      })
+      console.log('socket ok')
+    }
+    console.log(this.socket)
   },
   destroyed () {
     window.removeEventListener('resize', this.getWindowWidth)
